26647deb1f208aba42616f6dfa1ae7e4527f3822
shusiniski <anarak@code.edu.az>
2017-09-20 18:18:16 +0400

    done

---
+++ js/scrpt.js
@@ -0,0 +1,77 @@
+// var obj=();
+// var obj01=Object.create();
+// var obj02=new SampleFunc();
+
+// obj={
+// 	runLolaRun:function(){
+// 		a=5;
+// 	}
+// }
+// abj.runLolaRun();
+// console.log(a);
+
+
+
+
+// obj={
+// 	a:{
+// 		x:this;
+// 	},
+// 	b:[1,2,3,this
+// 	]
+// }
+
+
+
+// function Ata(ataAdi){
+// 	this.ataAdi=ataAdi;
+// 	this.soyad="Karimov";
+// 	 this.miras=false;
+	
+
+// }
+
+// // var samir=new Ata();
+// function Ovlad(ad){
+// 	this.ad=ad;
+// }
+// var samir=new Ata("Samir");
+// Ovlad.prototype=samir;
+
+// Ovlad.prototype=new Ata();       
+
+// var kamal=new Ovlad("Kamal");               
+
+// var leyla=new Ovlad("Leyla");
+
+
+
+
+// kamal 
+// Ovlad {ad: "Kamal"}ad: "Kamal"__proto__: Ata
+// kamal._proto_
+// undefined
+// kamal.__proto__
+// Ata {soyad: "Karimov", miras: false}miras: falsesoyad: "Karimov"__proto__: Object
+// kamal.__proto__.__proto__
+// {constructor: ƒ}constructor: ƒ Ata()__proto__: Object
+// kamal.__proto__.__proto__.__proto__
+// {__defineGetter__: ƒ, __defineSetter__: ƒ, hasOwnProperty: ƒ, __lookupGetter__: ƒ, __lookupSetter__: ƒ, …}
+
+
+// Ovlad.prototype=new Ata();        Ovlad-burda qelib
+
+// var kamal=new Ovlad("Kamal");               obyekt(miras almaq)
+
+// var leyla=new Ovlad("Leyla")
+
+
+var tabs=document.getElementByID("tabs");
+var LiElement=tabs.getElementByTagname("li")
+var content=document.getElementByClasname("content")[0];
+var contentText=["test01","test02","test03","test04","test05"]
+function changeMe(a){
+	for ( i = 0; i < LiElement.leng; i++) {
+		Things[i]
+	}
+}
